16c7bc81ede2b1e971565c84e782d367f5b5da98
Malik Fareed <fareed2109496@gmail.com>
2023-02-21 23:10:03 +0500

    third commit

--- app/src/main/java/com/malik/whatsappclone/ChatActivity.java
+++ app/src/main/java/com/malik/whatsappclone/ChatActivity.java
@@ -36,8 +36,7 @@ public class ChatActivity extends AppCompatActivity {
     DatabaseReference mChatDb;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState)    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
